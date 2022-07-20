c7fd3aeb18b344d10bd3cecf3a0d5858c0b47378
Thiago Negreiros <negreiros.t@gmail.com>
2022-07-20 16:21:24 -0300

    first lesson

---
+++ src/screens/SingIn.tsx
@@ -0,0 +1,46 @@
+import { Heading, Icon, VStack, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
+import { useState } from "react";
+
+import Logo from "../assets/logo_primary.svg";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
+export function SingIn() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const { colors } = useTheme();
+
+  function handleSingIn() {
+    console.log(email, pass);
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg={"gray.600"} px={8} pt={24}>
+      <Logo />
+
+      <Heading color={"gray.100"} fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
+      />
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPass}
+      />
+
+      <Button title="Entrar" w="full" onPress={handleSingIn} />
+    </VStack>
+  );
+}
